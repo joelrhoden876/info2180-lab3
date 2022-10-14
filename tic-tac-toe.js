b7198756ebc016ceb5f1e6e38939b1c232e34701
Joel Rhoden <joelrhodenjm@gmail.com>
2022-10-14 00:20:18 -0500

    Edited  javascript file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,25 @@
+X = true;
 window.onload = function(){
     let board = document.getElementById("board");
     let squares = document.querySelectorAll("#board div");
-
+    //let X = "X";
     for (let s in squares){
         squares[s].classList.add("square");
+        squares[s].setAttribute("class","square");
+        squares[s].addEventListener("click", function(){
+            if (X == true){
+                squares[s].innerHTML = "X";
+                squares[s].classList.add("X");
+                X = false;
+            }
+            else{
+                squares[s].innerHTML = "O";
+                squares[s].classList.add("O");
+                X = true;
+            }
+        })
+        
     }
+    
 }
 
-//div.className = "square";
